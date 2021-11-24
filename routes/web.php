4bc660d7d10f2363e5e55fc1cd9d4a24b9ba8375
koya1616 <87183507+koya1616@users.noreply.github.com>
2021-11-24 22:24:49 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,9 @@ Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->
 
 Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
+
+Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
+
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
